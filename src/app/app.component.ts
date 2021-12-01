6cf45b1d144de2d1962b8e9d17e2c37932734a80
koolkamalkishor <kamalkishor.cse@gmail.com>
2021-12-01 17:58:05 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <input [(ngModel)]="mass" placeholder="mass" />
-  <input [(ngModel)]="height" placeholder="height" />
+  <input [(ngModel)]="mass" placeholder="mass in KG" />
+  <input [(ngModel)]="height" placeholder="height IN CM" />
   <div *ngIf="mass && height"> {{ bmi }} </div>
   `,
   styleUrls: ['./app.component.css'],
@@ -12,8 +12,8 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
 
-  mass = 0;
-  height = 0;
+  mass : any;
+  height :any;
 
   get bmi() {
     return   (this.mass / this.height / this.height )*10000
